fa5787d5293a361b605f582a7382a80a773990af
Martin <martin@clareandme.com>
2022-11-20 23:12:55 +0100

    set up app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { Container, CssBaseline, Paper, SxProps, Theme } from '@mui/material'
 import { FC } from 'react'
+import ControlPanel from './components/ControlPanel'
+import DrawingSpace from './components/DrawingSpace'
+import { useCanvas } from './hooks/useCanvas'
+
+const containerStyle: SxProps<Theme> = {
+  display: 'flex',
+  mt: 4,
+  height: '85vh',
+}
 
 const App: FC = () => {
-  return <div className="App">Hello world</div>
+  const { rects, color, rectCount, handleMouseDown, handleMouseMove, handleMouseUp, setColor, clearRects, setDrawing } =
+    useCanvas()
+
+  return (
+    <Container maxWidth="md">
+      <CssBaseline />
+      <Paper elevation={6} sx={containerStyle}>
+        <ControlPanel
+          color={color}
+          rectCount={rectCount}
+          setColor={setColor}
+          clearRects={clearRects}
+          setDrawing={setDrawing}
+        />
+        <DrawingSpace
+          rects={rects}
+          handleMouseDown={handleMouseDown}
+          handleMouseMove={handleMouseMove}
+          handleMouseUp={handleMouseUp}
+        />
+      </Paper>
+    </Container>
+  )
 }
 
 export default App
